1e879d1e8894db8e0d80036434c276084d688e29
Vinay Avasthi <avasthi.vinay88@gmail.com>
2022-06-27 11:17:22 +0530

    add config folder used in sed example

---
+++ config/config.template.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: <DEBUG>,
+    environmentName: '<ENV>',    
+    apiGatewayHostName: '<APIHOST>/',
+    VA_PORTAL_URL: 'https://<VAHOST>/admin',
+    VA_LAUNCHPAD_URL: 'https://<VAHOST>/admin/launchpad',
+    PORTAL_HELPDESK: '<PORTAL_HELPDESK>',
+    VA_LOGIN: 'https://<VAHOST>/admin',
+    vaBaseUrl: 'https://<VAHOST>'    
+};
